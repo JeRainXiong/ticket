84fa7b47a2e6c7d266cfb47f376117cfb065a58b
JerryXiong12 <874919605@qq.com>
2018-06-14 15:33:24 +0800

    更新6.14

--- public/index.php
+++ public/index.php
@@ -13,6 +13,21 @@ define("LIBRARY_PATH",  APP_PATH . "application/library/");
 define("CONCTROLLER_PATH",  APP_PATH . "application/controllers/");
 
 isset($_SESSION) || session_start();
+$origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : ''; 
+    header("Access-Control-Allow-Origin: {$origin}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');
 
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
 $app = new Yaf_Application(APP_PATH . "/conf/application.ini","product");
 $app->bootstrap()->run();
